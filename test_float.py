0c1eb9ddf523bc0256d51cefc839184435437de1
Sobenin Anton <toshkasob@users.noreply.github.com>
2021-09-24 17:50:26 +0300

    Create test_float.py

---
+++ test_float.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_equal():
+    assert 5.0 / 10.0 = 0.5
+
+def test_notequal():
+    assert 3.0 / 10.0 != 3.0
+
+def test_less():
+    assert 3.0 < 3.000001
+
+def test_greater():
+    assert 5.0 - 4.999999 > 0
+
+@pytest.mark.parametrize("inNumb", [-1000.5, -1.1, 0.0, 1.1, 1000.5 ])
+def test_eval(inNumb):
+    assert inNumb * 0 == 0
