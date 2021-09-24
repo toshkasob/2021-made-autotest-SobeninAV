fb4fd5326bfd435496208359976ea75b2620a164
Sobenin Anton <toshkasob@users.noreply.github.com>
2021-09-24 17:59:55 +0300

    add test_error()

--- test_int.py
+++ test_int.py
@@ -14,5 +14,11 @@ def test_greater():
     assert 5 > 3
 
 @pytest.mark.parametrize("inNumb", [-1000, -1, 0, 1, 1000 ])
-def test_eval(inNumb):
+def test_multzero(inNumb):
     assert inNumb * 0 == 0
+
+def test_error():
+    try:
+        assert math.exp(1000)
+    except OverflowError:
+        pass
