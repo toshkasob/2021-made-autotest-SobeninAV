ea9ef72785e399132ffcd01492cd5b1f59494130
Sobenin Anton <toshkasob@users.noreply.github.com>
2021-09-24 18:06:16 +0300

    Add test_error

--- test_float.py
+++ test_float.py
@@ -1,4 +1,5 @@
 import pytest
+import math
 
 
 def test_equal():
@@ -14,5 +15,11 @@ def test_greater():
     assert 5.0 - 4.999999 > 0
 
 @pytest.mark.parametrize("inNumb", [-1000.5, -1.1, 0.0, 1.1, 1000.5 ])
-def test_eval(inNumb):
+def test_multzero(inNumb):
     assert inNumb * 0 == 0
+
+def test_error():
+    try:
+        assert 0.000001 / math.exp(1000)
+    except FloatingPointError:
+        pass
